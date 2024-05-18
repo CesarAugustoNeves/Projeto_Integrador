486a3a3df46e5675c462c12a23149dd9742c1e19
viniciusbuenno <viniciusbueno123456@gmail.com>
2024-05-17 21:30:03 -0300

    Criação da tabela e entrada de dados

--- produtos.sql
+++ produtos.sql
@@ -1,12 +1,28 @@
-CREATE TABLE PRODUTOS(
+USE projeto_integrador_pucc;
+
+CREATE table PRODUTOS(
 codigo int not null auto_increment,
-nome varchar(30),
+nome varchar(50),
 descricao varchar(100),
-custo decimal(9,2),
-cf decimal(4,2),
-cv decimal(4,2),
-impostos decimal(10,2),
-rentabilidade decimal(4,2),
-primary key(codigo))
+cp decimal(10,2),
+cf decimal(10,2),
+cv decimal(10,2),
+iv decimal(10,2),
+ml decimal(10,2),
+PRIMARY KEY (codigo))
 default charset=utf8mb4;
+
+INSERT INTO PRODUTOS
+VALUES(
+'1', 'Mesa', 'Mesa de escritório feita de madeira', '75.00', '11.25',
+'3.75', '9.00', '15.00'); 
+
+
+
+
+
+INSERT INTO PRODUTOS
+VALUES
+('2', 'Porta', 'Porta de ferro', '50.00', '17.00', '6.00', '20.00', '30.00');
+
 
