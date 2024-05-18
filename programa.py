ca044ee8d0c8b91026d395b0ba07e86e3f4c0827
viniciusbuenno <viniciusbueno123456@gmail.com>
2024-05-17 21:31:05 -0300

    Versão 2.0

--- programa.py
+++ programa.py
@@ -19,66 +19,109 @@ import mysql.connector
 db = mysql.connector.connect(
     host = "127.0.0.1",
     user = "root",
-    password = "codifique")
+    password = "codifique",
+    database = "projeto_integrador_pucc")
 
-print(db.is_connected()) #mostra se conectou
+while db.is_connected == False: 
+    print("Erro na conexão com banco de dados, tente corrigir os dados!")
+    break
 
 cursor = db.cursor()
 
-cursor.execute("describe projeto_integrador_pucc.PRODUTOS")
 
-for i in cursor:
-    print(i) #printando o que o sql retornou do execute
 
-passou = False
+def qualCodigo():
 
-while passou == False:
-    try:
-        codigo = int(input("Insira o código do produto: "))
+    passou = False
+
+    while passou == False:
+        try:
+            codigo = int(input("Insira o código do produto: "))
         
-    except ValueError:
-        print("O Codigo deve ser um número inteiro")
-    else:
-        passou = True
-        
+        except ValueError:
+            print("O Codigo deve ser um número inteiro")
+        else:
+            passou = True
+    return codigo
+
+
+def executarDb(comando, usaFetch=True):
+    print(f"Executando: {comando}\n\n")
+
+    cursor.execute(comando)
+
+    print("Função executada!\n")
+
+    if usaFetch == True:
+        dadosSelect = cursor.fetchall()
+        return dadosSelect
+    
+    
+def calcularLucro(dados):
+    
+    erro = True
+
+    while erro == True:
+        try:
+            cp = dados[3]
+            cf = dados[4]
+            cv = dados[5]
+            iv = dados[6]
+            ml = dados[7]
+        except IndexError: #Esse erro acontece quando tentamos passar a lista inteira (dados) para essa função ao invés da tupla que está dentro dela (dados[0])
+            print("Erro ao passar valores da tabela para as variáveis")  
+            break
+        else:  
+            erro = False
+    
+    
+            pv = cp / (1 - (cf + cv + iv + ml) / 100)
+
+            receita = pv - cp
+
+            outrosCustos = (cf / 100 * pv) + (cv / 100 * pv) + (iv / 100 * pv)
+
+            rentabilidade = receita - outrosCustos
+
+            lucro = rentabilidade / 100
+
+            print(f"Descrição                            Valor    %")
+            print(f"A. Preço de Venda                    {round(pv, 2)}   {round((pv / pv) * 100, 2)}%")
+            print(f"B. Custo de Aquisição (fornecedor)   {round(cp, 2)}   {round((cp/ pv) * 100, 2)}%")
+            print(f"C. Receita Bruta (A-B)               {round(receita, 2)}   {round((receita / pv) * 100, 2)}%")
+            print(f"D. Custo Fixo/Administrativo         {round(cf / 100 * pv, 2)}   {round(cf, 2)}%")
+            print(f"E. Comissão de Vendas                {round(cv/ 100 * pv, 2)}   {round(cv, 2)}%")
+            print(f"F. Impostos                          {round(iv/ 100 * pv, 2)}   {round(iv, 2)}%")
+            print(f"G. Outros custos (D+E+F)             {round(outrosCustos, 2)} {round((outrosCustos / pv) * 100, 2)}%")
+            print(f"H. Rentabilidade (C - G)             {round(rentabilidade, 2)} {round((rentabilidade/ pv) * 100, 2)}%\n\n")
+
+
+        if lucro < 0:
+            print("O seu lucro é menor do que 0, logo você está em PREJUÍZO!")
+        elif lucro == 0:
+            print("O seu lucro é de 0%, logo você está em EQUILÍBRIO")
+        elif lucro > 0 and lucro < 10 / 100:
+            print("O seu lucro está entre 0 e 10%, logo ele é BAIXO")
+        elif lucro > 10/100 and lucro < 20/100:
+            print("O seu lucro está entre 10% e 20%, logo ele é MÉDIO")
+        elif lucro > 20 / 100:
+            print("O seu lucro é maior que 20%, logo ele é ALTO")
 
 
 
-pv = cp / (1 - (cf + cv + iv + ml) / 100)
 
-receita = pv - cp
-
-outrosCustos = (cf / 100 * pv) + (cv / 100 * pv) + (iv / 100 * pv)
-
-rentabilidade = receita - outrosCustos
+# Definições acima ^^^
 
 
+codigo = qualCodigo()
+
+dados = executarDb(f"SELECT * FROM PRODUTOS WHERE CODIGO = {codigo}", True) #Executando o comando no DB e depois usando fetchall()
+
+#Retorna uma lista com uma tupla dentro, sendo cada item da tupla um dado da tabela.
 
 
-lucro = rentabilidade / 100
 
-print(f"Descrição                            Valor    %")
-print(f"A. Preço de Venda                    {round(pv, 2)}   {round((pv / pv) * 100, 2)}%")
-print(f"B. Custo de Aquisição (fornecedor)   {round(cp, 2)}   {round((cp/ pv) * 100, 2)}%")
-print(f"C. Receita Bruta (A-B)               {round(receita, 2)}   {round((receita / pv) * 100, 2)}%")
-print(f"D. Custo Fixo/Administrativo         {round(cf / 100 * pv, 2)}   {round(cf, 2)}%")
-print(f"E. Comissão de Vendas                {round(cv/ 100 * pv, 2)}   {round(cv, 2)}%")
-print(f"F. Impostos                          {round(iv/ 100 * pv, 2)}   {round(iv, 2)}%")
-print(f"G. Outros custos (D+E+F)             {round(outrosCustos, 2)} {round((outrosCustos / pv) * 100, 2)}%")
-print(f"H. Rentabilidade (C - G)             {round(rentabilidade, 2)} {round((rentabilidade/ pv) * 100, 2)}%")
-
-
-if lucro < 0:
-    print("O seu lucro é menor do que 0, logo você está em PREJUÍZO!")
-elif lucro == 0:
-    print("O seu lucro é de 0%, logo você está em EQUILÍBRIO")
-elif lucro > 0 and lucro < 10 / 100:
-    print("O seu lucro está entre 0 e 10%, logo ele é BAIXO")
-elif lucro > 10/100 and lucro < 20/100:
-    print("O seu lucro está entre 10% e 20%, logo ele é MÉDIO")
-elif lucro > 20 / 100:
-    print("O seu lucro é maior que 20%, logo ele é ALTO")
-
+calcularLucro(dados[0]) #Se passar a lista inteira dará erro, usar dados[i] para passar a tupla dentro da lista
 
 
 
